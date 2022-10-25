317578282f2332122333faa0020776147af00a1b
123ayush-abs <sarafayush130@gmail.com>
2022-10-25 11:49:37 +0530

    First Commit

---
+++ Cinemaseat.py
@@ -0,0 +1,47 @@
+class Movie:
+    show1seat=["A1","A2","A3","A4","A5","A6","A9"]
+    show2seat=["A1","A2","A4","A5","A6","B1","B3","B4","B5","B6","B7"]
+    
+    @staticmethod   #utility method to perform only calculation
+    def Iseatthere(booked_seat,avail_seat):  #to check if seat is available
+        avail_set=set(avail_seat)
+        booked_seat=set(booked_seat)
+        res_set=avail_set.union(booked_seat)
+        if(len(res_set)==len(avail_seat)):
+            return 1
+        else:
+            return 0  
+    
+class Show1:
+    def __init__(self,bookedseat):
+        self.bookedseat=bookedseat
+    def Reamin_Seat(self):
+        for seat in self.bookedseat:
+           Movie.show1seat.remove(seat)
+        
+    
+class Show2:
+    def __init__(self,bookedseat):
+        self.bookedseat=bookedseat
+    def Reamin_Seat(self):
+        for seat in self.bookedseat:
+            Movie.show2seat.remove(seat)
+
+print("Menue:--")
+print("Press 0 to quit")
+while(1):
+    show_num=int(input("Enter Show Number"))
+    seat_req=list(map(str,input("Enter Seats Required:").split()))
+    if(show_num==1 and Movie.Iseatthere(seat_req, Movie.show1seat)==1):
+        print("Booked!!")
+        obj1=Show1(seat_req)
+        obj1.Reamin_Seat()
+        print(Movie.show1seat)
+        
+    elif(show_num==2 and Movie.Iseatthere(seat_req, Movie.show2seat)==1):
+        print("Booked for SHow2::")
+        obj2=Show2(seat_req)
+        obj2.Reamin_Seat()
+        print(Movie.show2seat)
+    elif(show_num==0):
+        break
