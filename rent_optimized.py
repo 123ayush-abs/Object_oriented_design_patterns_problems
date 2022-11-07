ccf6de83c04f32c466180aa8e29558374016fff2
123ayush-abs <74004844+123ayush-abs@users.noreply.github.com>
2022-11-07 09:32:27 +0530

    Update rent_optimized.py

--- rent_optimized.py
+++ rent_optimized.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
+from abc import ABC,abstractmethod
 #stratedy method
-def Suggest_Driver_withoutdest(car_dict,cab_name):
+def Suggest_Driver(car_dict,cab_name,cab_dest=None):
         if(cab_name not in car_dict):
             return "Select Another Car Sir!!"
         else:
@@ -11,55 +12,27 @@ def Suggest_Driver_withoutdest(car_dict,cab_name):
                 if(float(driver[1])<4):
                     continue 
                 else:
-                    if(driver[2]<=min_dist and float(driver[1])>=4):
+                    if(driver[2]<=min_dist and cab_dest is None):
                         min_dist=driver[2]
                         driver_name=driver[0]
-                       
-                    else:
-                        pass 
-       
+                    elif(driver[2]<=min_dist and cab_dest is  not None and cab_dest in driver[3]):
+                        min_dist=driver[2]
+                        driver_name=driver[0]
         if(len(driver_name)==0 ):
-            return "Could not find driver"
+            return "Could not find suitabel driver for your destination"
         return driver_name
-def Suggest_Driver_dest(car_dict,cab_name,cab_dest):
-        if(cab_name not in car_dict):
-            return "Select Another Car Sir!!"
-        else:
-          
-            driver_name=''
-            min_dist=10**9
-            for driver in car_dict[cab_name]:
-                if(float(driver[1])<4):
-                    continue 
-                else:
-                    if(driver[2]<=min_dist and cab_dest in driver[3]):
-                        min_dist=driver[2]
-                        driver_name=driver[0]
-                        
-                    else:
-                        pass
-        if(len(driver_name)==0 ):
-            return "Could not find driver"
-        return driver_name 
-
-
-# class Caluclate_Fair(ABC):
-#     @abstractmethod
-#     def Calculate_Fair_taxi(self,cust_distance):
-#         pass 
+class Caluclate_Fair(ABC):
+    @abstractmethod
+    def Calculate_Fair_taxi(self,cust_distance):
+        pass 
 class Driver_Suggestion:
     def __init__(self,car_dict,strategy=None):
         self.car_dict=car_dict
         self.strategy=strategy
       
     def Get_Driver_Status(self,cab_name,dest=None):
-     
-        if(self.dest is None):
-            self.strategy=Suggest_Driver_withoutdest
-            driver_name=self.strategy(self.car_dict,cab_name)
-        else:
-            self.strategy=Suggest_Driver_dest
-            driver_name=self.strategy(self.car_dict,cab_name,dest)
+        self.strategy=Suggest_Driver
+        driver_name=self.strategy(self.car_dict, cab_name,dest)
         return driver_name
             
         
@@ -71,16 +44,17 @@ class Register_Cab(Driver_Suggestion):
         self.cab_model=cab_model
         self.rating=rating
         if(dest is not None):
-            self.dest=dict.fromkeys(dest,0)
+            self.dest=set(dest)
         else:
             self.dest=dest
         self.dfc=dfc 
         Register_Cab.cab_dict[self.cab_model].append([self.driver_name,self.rating,self.dfc,self.dest])
         Driver_Suggestion.__init__(self,  Register_Cab.cab_dict)
-       
-    @staticmethod  #utility method
-    def Calculate_Fair_taxi(cust_distance):
+    def Calculate_Fair_taxi(self,cust_distance):
         return cust_distance*8
+    # @staticmethod  #utility method
+    # def Calculate_Fair_taxi(cust_distance):
+    #     return cust_distance*8
     
     
 # #input
@@ -122,27 +96,27 @@ class Register_Cab(Driver_Suggestion):
 # print(driver_status)
 
 # # # #test case 2;---
-# taxi=Register_Cab('A', '5SEATER', 4 ,500,["Gurgaon", "Noida", "Delhi"])
-# taxi=Register_Cab('B' ,'HatchBack' ,4.3, 1000,["Gurgaon"])
-# taxi=Register_Cab('C','5SEATER',4.8,200,["Noida"])
-# taxi=Register_Cab('D','Sedan',4.1,700,["Noida"])
-# taxi=Register_Cab('E','5SEATER',4.7,430,["Delhi"])
-# fair=taxi.Calculate_Fair_taxi(60)
-# driver_status=taxi.Get_Driver_Status("5SEATER","Delhi")
-# print(fair)
-# print(driver_status)
+taxi=Register_Cab('A', '5SEATER', 4 ,500,["Gurgaon", "Noida", "Delhi"])
+taxi=Register_Cab('B' ,'HatchBack' ,4.3, 1000,["Gurgaon"])
+taxi=Register_Cab('C','5SEATER',4.8,200,["Noida"])
+taxi=Register_Cab('D','Sedan',4.1,700,["Noida"])
+taxi=Register_Cab('E','5SEATER',4.7,430,["Delhi"])
+fair=taxi.Calculate_Fair_taxi(60)
+driver_status=taxi.Get_Driver_Status("5SEATER","Delhi")
+print(fair)
+print(driver_status)
 # # print(Register_Cab.cab_dict)
 
 # # test case 3
-taxi=Register_Cab('A', 'Sedan', 4 ,500)
-taxi=Register_Cab('B' ,'HatchBack' ,4.3, 1000)
-taxi=Register_Cab('C','5SEATER',4.8,200)
-taxi=Register_Cab('D','Sedan',4.1,700)
-taxi=Register_Cab('E','HatchBack',4.8,430)
-fair=taxi.Calculate_Fair_taxi(20.5)
-driver_status=taxi.Get_Driver_Status('HatchBack')
-print(fair)
-print(driver_status)
+# taxi=Register_Cab('A', 'Sedan', 4 ,500)
+# taxi=Register_Cab('B' ,'HatchBack' ,4.3, 1000)
+# taxi=Register_Cab('C','5SEATER',4.8,200)
+# taxi=Register_Cab('D','Sedan',4.1,700)
+# taxi=Register_Cab('E','HatchBack',4.8,430)
+# fair=taxi.Calculate_Fair_taxi(20.5)
+# driver_status=taxi.Get_Driver_Status('HatchBack')
+# print(fair)
+# print(driver_status)
 
 
     
